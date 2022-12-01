bc84da33177e9b7c2a18644cf4cdea0a15d416f0
vaggelisdel <dellios@oktopay.eu>
2022-12-01 22:03:00 +0200

    concurrently package

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,19 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import entriesReducer from '../reducers/entries.reducer';
 import modalsReducer from '../reducers/modals.reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from '@redux-saga/core';
+
+const sagaMidddleware = createSagaMiddleware();
+const middlewares = [sagaMidddleware];
 
 const configureStore = () => {
     return createStore(combineReducers({
         entries: entriesReducer,
         modals: modalsReducer
-    }), composeWithDevTools()
+    }), composeWithDevTools(
+        applyMiddleware(...middlewares)
+    )
 )}
 
 export default configureStore;
