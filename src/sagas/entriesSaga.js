23a8fbd69774c443079b380401d84cf4af9270c2
vaggelisdel <dellios@oktopay.eu>
2022-12-05 00:23:26 +0200

    Saga fork

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -9,3 +9,16 @@ export function* getAllEntries() {
     console.log(result);
     yield put({type: entriesTypes.POPULATE_ENTRIES, payload: result.data})
 };
+
+export function* getEntryDetails(id){
+    const {data} = yield call(axios, `http://localhost:3001/value/${id}`)
+    yield put({type: entriesTypes.POPULATE_ENTRY_DETAILS, payload: {id, entry:data}})
+}
+
+export function* getAllEntiesyDetails(id){
+    const {payload} = yield take(entriesTypes.POPULATE_ENTRIES);
+    for (let index = 0; index < payload.length; index++) {
+        const entry = payload[index];
+        yield fork(getEntryDetails, entry.id);   
+    }
+}
